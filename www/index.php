bf0914c57914521d212ed0fd69f351566ddde723
DaryaSukhanova <daria_15tit@mail.ru>
2023-02-19 14:13:44 +0300

    commit

--- www/index.php
+++ www/index.php
@@ -13,32 +13,26 @@
    <h1>Матрица A</h1>
    <form  method="POST" action="">
       <div class="matrix">
-         
-         <input autocomplete="off"  step="any" type="number" name="element11" required>
-         <input autocomplete="off"  step="any" type="number" name="element12" required>
-         <input autocomplete="off"  step="any" type="number" name="element13" required>
-         <input autocomplete="off"  step="any" type="number" name="element21" required>
-         <input autocomplete="off"  step="any" type="number" name="element22" required>
-         <input autocomplete="off"  step="any" type="number" name="element23" required>
-         <input autocomplete="off"  step="any" type="number" name="element31" required>
-         <input autocomplete="off"  step="any" type="number" name="element32" required>
-         <input autocomplete="off"  step="any" type="number" name="element33" required>
+         <?php 
+            for ($i = 1; $i<=9; $i++){
+               echo("<input autocomplete='off'  step='any' type='number' name='element".$i."' required>");
+            } 
+         ?>
   </div>
       <input type="submit" value="Вычислить определитель матрицы" class="button">
       <input type="reset" class="button">
    </form>
 
    <?php 
-
-   // $element11 =  $_POST['element11'];is_numeric
-   $element12 = is_numeric($_POST['element12']) ? $_POST['element12']: 0;
-   $element13 = is_numeric($_POST['element13']) ? $_POST['element13']: 0;
-   $element21 = is_numeric($_POST['element21']) ? $_POST['element21']: 0;
-   $element22 = is_numeric($_POST['element22']) ? $_POST['element22']: 0;
-   $element23 = is_numeric($_POST['element23']) ? $_POST['element23']: 0;
-   $element31 = is_numeric($_POST['element31']) ? $_POST['element31']: 0;
-   $element32 = is_numeric($_POST['element32']) ? $_POST['element32']: 0;
-   $element33 = is_numeric($_POST['element33']) ? $_POST['element33']: 0;
+   $element11 = is_numeric($_POST['element1']) ? $_POST['element1']: 0;
+   $element12 = is_numeric($_POST['element2']) ? $_POST['element2']: 0;
+   $element13 = is_numeric($_POST['element3']) ? $_POST['element3']: 0;
+   $element21 = is_numeric($_POST['element4']) ? $_POST['element4']: 0;
+   $element22 = is_numeric($_POST['element5']) ? $_POST['element5']: 0;
+   $element23 = is_numeric($_POST['element6']) ? $_POST['element6']: 0;
+   $element31 = is_numeric($_POST['element7']) ? $_POST['element7']: 0;
+   $element32 = is_numeric($_POST['element8']) ? $_POST['element8']: 0;
+   $element33 = is_numeric($_POST['element9']) ? $_POST['element9']: 0;
    
    $array =array(1 => array(1 => $element11, $element12, $element13), array(1 => $element21, $element22, $element23), array(1 => $element31, $element32, $element33));
    
@@ -48,19 +42,7 @@
    $res = $element11 * $element22 * $element33 + $element12 * $element23 * $element31 + $element13 * $element21 * $element32 - $element13 * $element22 * $element31 - $element11 * $element23 * $element32 - $element12 * $element21 * $element33;
    
    if (!empty($_POST)){
-      // $content = "<div class='container'>
-      //    <div class='res-matrix'>
-      //    <div>".$element11."</div>
-      //    <div>".$element12."</div>
-      //    <div>".$element13."</div>
-      //    <div>".$element21."</div>
-      //    <div>".$element22."</div>
-      //    <div>".$element23."</div>
-      //    <div>".$element31."</div>
-      //    <div>".$element32."</div>
-      //    <div>".$element33."</div>
-      //    </div>
-      // </div>";
+
       
       ?>
       <div class="content">
@@ -114,20 +96,6 @@
 
          $result = "<div class='string'> <span>".$string1."</span>".$content."<span>".$string2."</span></div>";
          echo $result;
-         
-         // for($i=0; $i < $count; $i++) {
-         //    if ($array[$i] === ''){
-         //       echo 'Поле не заполнено'.'<br/>';
-         //       exit();
-         //    } else{
-         //       echo $array[$i].'<br/>';
-         //    }
-         //    // echo gettype($array[$i]);
-            
-         // }
-
-         
-
       }
    ?>
 
@@ -141,7 +109,6 @@
                      Транспонированная матрица
                      <?php
                   }
-                  
                   ?>
                   
                </summary>
